27c8039b41f6b2cd6a63707b632c575ee4b94acb
H20Beetle <waterbeetle091@gmail.com>
2023-09-07 19:24:08 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+// Exercise #1
+
+// function outerFunction(){
+//     let outerVar = "I'm outside!";
+
+//      function innerFunction() {
+//         let innerVar = "I'm inside!";
+//         console.log(outerVar);
+//         console.log(innerVar);
+//     }
+
+//     innerFunction();
+// }
+
+// outerFunction();
+
+
+
+
+
+//Exerrise #2
+
+mainFunction('Kyle');
+
+function mainFunction(myName){
+    console.log(`Hello ${myName}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
