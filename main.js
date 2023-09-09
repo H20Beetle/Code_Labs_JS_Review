9997a9077dc153770d994e781ebc96dcf360b2cd
H20Beetle <waterbeetle091@gmail.com>
2023-09-09 04:30:30 -0500

    update on work

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 // Exercise #1
 
 // function outerFunction(){
-//     let outerVar = "I'm outside!";
+//     let outerVar = "I'm outside!";  
 
 //      function innerFunction() {
-//         let innerVar = "I'm inside!";
+//         let innerVar = "I'm inside!";  // inside function can access the outer function 
 //         console.log(outerVar);
 //         console.log(innerVar);
 //     }
@@ -18,13 +18,54 @@
 
 
 
-//Exerrise #2
+//Exercise #2
 
-mainFunction('Kyle');
+const myName = 'Kyle'
 
-function mainFunction(myName){
-    console.log(`Hello ${myName}`);
+mainFunction(myName);
+
+function mainFunction(myName) {
+	console.log(`${myName} is learning to code!`);
 }
+
+// Exercise #3
+
+function outer(){
+    let myPhoneNumber = 4178652689
+    function inner(){
+        console.log('I would like to call', myPhoneNumber);
+    }
+    return inner;
+}
+
+const callPhone = outer();
+
+callPhone();
+
+// Exercise #4
+
+let catsName = 'Freya';
+
+function modifyGlobal() {
+    console.log(catsName);
+}
+
+function localScopeTest() {
+    catsName = 'Etna';
+   console.log(catsName);
+}
+
+modifyGlobal();
+localScopeTest();
+
+// Exericise #5 
+
+
+
+
+
+
+
 
 
 
